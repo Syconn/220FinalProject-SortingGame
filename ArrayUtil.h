2e0d877ca09dc417397477bcf719d0d1103d869f
Syconn <49461127+Syconn@users.noreply.github.com>
2025-10-06 01:29:17 -0400

    Initial commit

---
+++ ArrayUtil.h
@@ -0,0 +1,12 @@
+//
+// Created by Aidan on 10/6/2025.
+//
+
+#ifndef INC_220FINALMYVERSION_ARRAYUTIL_H
+#define INC_220FINALMYVERSION_ARRAYUTIL_H
+
+bool isSorted(const int arr[], int len);
+void printArray(const int arr[], int len);
+void swap(int arr[], int i, int j);
+
+#endif //INC_220FINALMYVERSION_ARRAYUTIL_H
---
+++ Sorter.h
@@ -0,0 +1,18 @@
+//
+// Created by Aidan on 10/2/2025.
+//
+
+#ifndef INC_220FINALMYVERSION_SORTER_H
+#define INC_220FINALMYVERSION_SORTER_H
+
+#include <iostream>
+
+void selectionSort(int arr[], int size);
+void bubbleSort(int arr[], int size);
+void insertionSort(int arr[], int size);
+void quickSort(int arr[], int low, int high);
+void mergeSort(int arr[], int low, int high);
+void bucketSort(int arr[], int size);
+void radixSort(int arr[], int size);
+
+#endif //INC_220FINALMYVERSION_SORTER_H
